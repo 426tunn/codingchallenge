ac879120acb1e6ff8a321bb423474ab80a2f9c96
426tunn <akanniagbolahan@gmail.com>
2024-05-27 14:27:11 +0100

    implement error handling and add static frontend

--- backend-code-challenge/junior/DepartureTimesChallenge/src/app.ts
+++ backend-code-challenge/junior/DepartureTimesChallenge/src/app.ts
@@ -3,15 +3,34 @@ import { Config } from "./Config/config";
 import { DepartureRouter } from "./Routes/DepartureRoute";
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './swagger';
+import cors from 'cors';
+import path from 'path';
 
 const PORT = Config.PORT;
 const app = express();
 app.use(express.json());
+app.use(cors()); 
 app.use(express.urlencoded({ extended: true }));
-app.use('/', express.static('static'));
+app.use(express.static(path.join(__dirname, 'static')));
+app.use('/', express.static('static')); 
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/departures", DepartureRouter); 
+
+//Error Handling
+app.use(
+  (
+      err: Error,
+      _req: express.Request,
+      res: express.Response,
+      next: express.NextFunction,
+  ) => {
+      res.status(500).json({
+          error: err.message,
+      });
+      next(err);
+  },
+);
 
 
 app.listen(PORT, () => {
